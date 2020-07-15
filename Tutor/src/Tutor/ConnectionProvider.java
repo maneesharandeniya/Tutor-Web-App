cc35036cede9df5053558129d3b95115b5992b6e
P.M.M.P Randeniya <maneesha@MANEESHA>
2020-07-16 00:26:58 +0530

    All files

---
+++ Tutor/src/Tutor/ConnectionProvider.java
@@ -0,0 +1,17 @@
+package Tutor;
+
+import java.sql.*;
+
+
+public class ConnectionProvider {	
+	public static Connection getCon() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Tutor","root","");
+		    return con;
+		}catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+}
